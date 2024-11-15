119ba0e02396c82b278b641d7f55531e6b21cb1f
sabyasachi <njoysubho@gmail.com>
2024-11-15 09:09:47 +0100

    Initional pipeline defintion

--- loan-approval-predictor-service/terraform/init.tf
+++ loan-approval-predictor-service/terraform/init.tf
@@ -1,13 +1,11 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.75.1"
-    }
-  }
   backend "s3" {
     bucket = "apps-tf"
-    key    = "${var.service_name}-${var.environment}.tfstate"
+    key    = "loan-approval-predictor-service.tfstate"
     region = "eu-west-1"
   }    
+}
+
+provider "aws" {
+  region = "eu-west-1"
 }
--- loan-approval-predictor-service/terraform/main.tf
+++ loan-approval-predictor-service/terraform/main.tf
@@ -3,8 +3,9 @@ data "local_file" "pipeline_definition" {
   filename = "${path.module}/pipeline_definition.json"
 }
 
-resource "aws_sagemaker_pipeline" "loan_approval_training_pipeline" {
+/** resource "aws_sagemaker_pipeline" "loan_approval_training_pipeline" {
      pipeline_name = "loan-approval-training-pipeline-${var.enviroment}"
      pipeline_display_name = "loan-approval-training-pipeline-${var.enviroment}"
      pipeline_definition = data.local_file.pipeline_definition.content
-}
+     role_arn = "arn:aws:iam::xxxx:role/SagemakerExecution"
+}**/
