f608236b15731ab6a25b1120bd99de8f40741581
sabyasachi <njoysubho@gmail.com>
2025-03-01 17:30:36 +0100

    Adding DB to litellm setup via supabase

--- litellm-gateway/runme.sh
+++ litellm-gateway/runme.sh
@@ -2,5 +2,6 @@
 docker run \
     -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
     -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}\
+    -e DATABASE_URL=${SUPABASE_LITE_LLM_DB_URL}\
     -p 4000:4000 \
     local-llm:latest
